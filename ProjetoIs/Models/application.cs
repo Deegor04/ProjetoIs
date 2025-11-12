7b42a28c37c97c5c944856d87d174443d7eb79f0
deegor <diogogomes600@gmail.com>
2025-11-12 16:28:43 +0000

    applicacion model

---
+++ ProjetoIs/Models/application.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations.Schema;
+using System.Linq;
+using System.Text.Json.Serialization;
+using System.Web;
+
+
+namespace ProjetoIs.Models
+{
+    public class application
+    {
+        [JsonPropertyName("resource-name")]
+        [Column("resource_name")]
+
+        public string ResourceName { get; set; }
+
+        [JsonPropertyName("res-type")]
+        [Column("res-type")]
+        public string ResType { get; set; }
+
+        [JsonPropertyName("creation-datetime")]
+        [Column("creation_datetime")]
+
+        public DateTime CreationDatetime { get; set; }
+    }
+}
