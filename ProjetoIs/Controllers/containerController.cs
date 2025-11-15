63da71eaebd6baa4be1ab91c8023da7cfca97fe8
deegor <diogogomes600@gmail.com>
2025-11-15 13:20:57 +0000

    Reestruturacao das classes

---
+++ ProjetoIs/Controllers/containerController.cs
@@ -0,0 +1,41 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net;
+using System.Net.Http;
+using System.Web.Http;
+
+namespace ProjetoIs.Controllers
+{
+    public class containerController : ApiController
+    {
+        string connectionString = System.Configuration.ConfigurationManager.ConnectionStrings["ProjetoIS.Properties.Settings.ConnectionString"].ConnectionString;
+
+        // GET: api/container
+        public IEnumerable<string> Get()
+        {
+            return new string[] { "value1", "value2" };
+        }
+
+        // GET: api/container/5
+        public string Get(int id)
+        {
+            return "value";
+        }
+
+        // POST: api/container
+        public void Post([FromBody]string value)
+        {
+        }
+
+        // PUT: api/container/5
+        public void Put(int id, [FromBody]string value)
+        {
+        }
+
+        // DELETE: api/container/5
+        public void Delete(int id)
+        {
+        }
+    }
+}
--- ProjetoIs/Models/container.cs
+++ ProjetoIs/Models/container.cs
@@ -1,18 +1,17 @@
 ﻿using System;
 using System.Collections.Generic;
-using System.ComponentModel;
 using System.ComponentModel.DataAnnotations;
 using System.ComponentModel.DataAnnotations.Schema;
 using System.Linq;
 using System.Web;
-//using System.Text.Json.Serialization;
 using Newtonsoft.Json;
-
+using static System.Net.Mime.MediaTypeNames;
 
 namespace ProjetoIs.Models
 {
-    public class application : common
+    public class container : common
     {
-        public virtual List<Container> Containers { get; set; } = new List<Container>();
+        [ForeignKey("application-resource-name")]
+        public virtual application ApplicationResourceName { get; set; }
     }
 }
