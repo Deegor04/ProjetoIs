ca5c4076ac9a4d0f85b270f76add8a7451434768
josebranco13 <161507217+josebranco13@users.noreply.github.com>
2025-11-26 03:30:05 +0000

    part1_subs

--- ProjetoIs/Controllers/subscriptionController.cs
+++ ProjetoIs/Controllers/subscriptionController.cs
@@ -1,4 +1,5 @@
-﻿using System;
+﻿using ProjetoIs.Models;
+using System;
 using System.Collections.Generic;
 using System.Configuration;
 using System.Linq;
@@ -8,15 +9,29 @@ using System.Web.Http;
 
 namespace ProjetoIs.Controllers
 {
-    [RoutePrefix("api/somiod/{applicationName}/{containerName}/sub")]
+    [RoutePrefix("api/somiod/{applicationName}/{containerName}/subs/{subName}")]
     public class subscriptionController : ApiController
     {
         string connectionString = ConfigurationManager.ConnectionStrings["ProjetoIs.Properties.Settings.ConnectionString"].ConnectionString;
 
         [HttpGet]
-        [Route("")]
-        public void Get()
+        [Route("{appName}/{containerName}/subs/{subName}")]
+        public IHttpActionResult GetSubscription(string appName, string containerName, string subName)
         {
+            var subscription = GetSubscriptionFromDb(appName, containerName, subName);
+            if (subscription == null)
+            {
+                return NotFound();
+            }
+
+            return Ok(subscription);
         }
+
+        public subscription GetSubscriptionFromDb(global::System.String appName, global::System.String containerName, global::System.String subName)
+        {
+            const string connDb = @"";
+        }
+
+
     }
 }
